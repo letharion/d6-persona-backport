bce0a4e5366004c996e3e5b584e495c9c19016dd
Daniel Pepin <donutdan4114@gmail.com>
2012-09-28 02:50:17 -0400

    By donutdan4114: Added admin form to customize button styles.

--- persona.js
+++ persona.js
@@ -16,7 +16,40 @@
         window.location = "/user/logout";
       });
       Drupal.persona.watch();
+      Drupal.persona.adminFormEnhance();
     }
+  }
+  
+  /**
+   * Enhances the admin form with live style updates.
+   */
+  Drupal.persona.adminFormEnhance = function(){
+    if($('#persona-admin-form').length < 1){
+      // Not on the admin form.
+      return FALSE;
+    }
+    var login_button = $('#edit-login-customize .persona-login');
+    var reg_button = $('#edit-register-customize .persona-login');
+    $('#edit-persona-login-button-style').change(function(e){
+      login_button.removeClass("dark orange persona-button");
+      if($(this).val()){
+        var button = "persona-button "; 
+      }
+      login_button.addClass(button + $(this).val());
+    });
+    $('#edit-persona-login-button-text').keyup(function(e){
+      login_button.children('span').text($(this).val());
+    });
+    $('#edit-persona-register-button-style').change(function(e){
+      reg_button.removeClass("dark orange persona-button");
+      if($(this).val()){
+        var button = "persona-button "; 
+      }
+      reg_button.addClass(button + $(this).val());
+    });
+    $('#edit-persona-register-button-text').keyup(function(e){
+      reg_button.children('span').text($(this).val());
+    });
   }
   
   Drupal.persona.watch = function(){
