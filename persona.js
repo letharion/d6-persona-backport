b9800e6fbd29cf72235015609dd0b2b254750c7a
Daniel Pepin <donutdan4114@gmail.com>
2012-10-01 04:12:30 -0400

    By donutdan4114: Trying to get code review clean.

--- persona.js
+++ persona.js
@@ -1,10 +1,10 @@
-(function($){
+(function($) {
 
   Drupal.persona = {}
 
   Drupal.behaviors.persona = {
-    attach:function(){
-      $('.persona-login').click(function(e){
+    attach:function() {
+      $('.persona-login').click(function(e) {
         e.preventDefault();
         e.stopPropagation();
         navigator.id.request({
@@ -12,7 +12,7 @@
           siteLogo:Drupal.settings.persona.site.logo
         });
       });
-      $('.persona-logout, a[href$="user/logout"], a[href$="index.php?q=user/logout"]').click(function(e){
+      $('.persona-logout, a[href$="user/logout"], a[href$="index.php?q=user/logout"]').click(function(e) {
         e.preventDefault();
         e.stopPropagation();
         navigator.id.logout();
@@ -26,39 +26,39 @@
   /**
    * Enhances the admin form with live style updates.
    */
-  Drupal.persona.adminFormEnhance = function(){
-    if($('#persona-admin-form').length < 1){
+  Drupal.persona.adminFormEnhance = function() {
+    if($('#persona-admin-form').length < 1) {
       // Not on the admin form.
       return false;
     }
     var login_button = $('#edit-login-customize .persona-login');
     var reg_button = $('#edit-register-customize .persona-login');
-    $('#edit-persona-login-button-style').change(function(e){
+    $('#edit-persona-login-button-style').change(function(e) {
       login_button.removeClass("dark orange persona-button");
-      if($(this).val()){
+      if($(this).val()) {
         var button = "persona-button ";
       }
       login_button.addClass(button + $(this).val());
     });
-    $('#edit-persona-login-button-text').keyup(function(e){
+    $('#edit-persona-login-button-text').keyup(function(e) {
       login_button.children('span').text($(this).val());
     });
-    $('#edit-persona-register-button-style').change(function(e){
+    $('#edit-persona-register-button-style').change(function(e) {
       reg_button.removeClass("dark orange persona-button");
-      if($(this).val()){
+      if($(this).val()) {
         var button = "persona-button ";
       }
       reg_button.addClass(button + $(this).val());
     });
-    $('#edit-persona-register-button-text').keyup(function(e){
+    $('#edit-persona-register-button-text').keyup(function(e) {
       reg_button.children('span').text($(this).val());
     });
   }
 
-  Drupal.persona.watch = function(){
+  Drupal.persona.watch = function() {
     navigator.id.watch({
       loggedInUser:Drupal.settings.persona.user.mail,
-      onlogin:function(assertion){
+      onlogin:function(assertion) {
         $.ajax({
           type:'POST',
           url:'index.php?q=persona/verify',
@@ -67,12 +67,12 @@
             token:Drupal.settings.persona.token
           },
           dataType:"json",
-          success:function(res, status, xhr){
+          success:function(res, status, xhr) {
             Drupal.persona.handleLogin(res);
           }
         });
       },
-      onlogout:function(){
+      onlogout:function() {
         $.ajax({
           type:'POST',
           url:'index.php?q=persona/logout'
@@ -81,16 +81,16 @@
     });
   }
 
-  Drupal.persona.handleLogin = function(res){
-    if(res.status === "okay"){
+  Drupal.persona.handleLogin = function(res) {
+    if(res.status === "okay") {
       // User should now be logged in, just refresh the page.
       window.location.reload();
-    }else if(res.status === "error"){
+    }else if(res.status === "error") {
       navigator.id.logout();
-      if(res.response === "needs to register"){
+      if(res.response === "needs to register") {
         window.location = Drupal.settings.basePath + "user/register";
       }
-      if(res.response === "access denied"){
+      if(res.response === "access denied") {
         window.location.reload();
       }
     }
