c9cb2ab93bfd21227d71111588f6c982529989f7
Jonathan Brown <jbrown@bluedroplet.com>
2013-04-01 00:42:45 +1100

    Improve admin JS.

--- persona.admin.js
+++ persona.admin.js
@@ -2,14 +2,14 @@
 
 Drupal.behaviors.personaAdmin = {
   attach: function (context, settings) {
-    var button_sign_in = $('.persona-sign-in');
-    $('#edit-persona-button-style').change(function (e) {
-      button_sign_in.removeClass("dark orange persona-button");
-      var button = "";
-      if ($(this).val()) {
-        button = "persona-button ";
+    var $button = $('.persona-sign-in');
+    $('#edit-persona-button-style').change(function (event) {
+      $button.removeClass('dark orange persona-button');
+      var buttonClass = $(this).val();
+      if (buttonClass) {
+        buttonClass += ' persona-button';
       }
-      button_sign_in.addClass(button + $(this).val());
+      $button.addClass(buttonClass);
     });
   }
 }
